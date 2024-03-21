3c82c2d6b42f28471abc8855eaa1974c5e870007
Samuel Sihotang <samsihotang1@gmail.com>
2024-03-21 08:32:27 +0700

    change username to not unique

--- models/User.go
+++ models/User.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	Model
-	Username string `gorm:"unique;not null"`
+	Username string `gorm:"not null"`
 	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
 	Photos   []Photo
